2c5d19249e3282b39d8d0d9d770f7f9ae767d1a8
Ayham <ayhampt011@gmail.com>
2026-08-15 10:16:51 +0530

    Added data ingestion module with MongoDB integration

---
+++ networksecurity/components/data_ingestion.py
@@ -0,0 +1,97 @@
+from networksecurity.exception.exception import customException
+from networksecurity.logging.logger import logging
+from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
+
+import sys
+import os
+import pymongo
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+from typing import List
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGO_URI")
+client = pymongo.MongoClient(MONGODB_URI)
+
+
+class DataIngestion:
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
+        try:
+            self.data_ingestion_config = data_ingestion_config
+
+        except Exception as e:
+            raise customException(e, sys) from e
+
+    """
+    Read data from MongoDB collection and export it as a DataFrame
+    """
+
+    def export_collection_as_dataframe(self):
+        try:
+            database_name = self.data_ingestion_config.database_name
+            collection_name = self.data_ingestion_config.collection_name
+            self.mongo_client = pymongo.MongoClient(MONGODB_URI)
+            collection = self.mongo_client[database_name][collection_name]
+            df = pd.DataFrame(list(collection.find()))
+            if "_id" in df.columns:
+                df = df.drop(columns=["_id"])
+            df.replace({"nan": np.nan}, inplace=True)
+            logging.info(
+                f"Exporting collection: {collection_name} from database: {database_name}"
+            )
+            return df
+
+        except Exception as e:
+            raise customException(e, sys) from e
+
+    """
+    Export DataFrame to the feature store directory
+    """
+
+    def export_data_to_feature_store(self, df: pd.DataFrame):
+        try:
+            feature_store_dir = self.data_ingestion_config.feature_store_dir
+            os.makedirs(feature_store_dir, exist_ok=True)
+            feature_store_file_path = os.path.join(
+                feature_store_dir, "feature_store.csv"
+            )
+            df.to_csv(feature_store_file_path, index=False, header=True)
+            return df
+        except Exception as e:
+            raise customException(e, sys) from e
+
+    """Train test split of the data and export it to the ingested directory"""
+
+    def split_data_as_train_test(self, df: pd.DataFrame):
+        try:
+            train_test_split_ratio = self.data_ingestion_config.train_test_split_ratio
+
+            train_set, test_set = train_test_split(
+                df, train_size=train_test_split_ratio, random_state=42
+            )
+            logging.info("Performed train test split")
+
+        except Exception as e:
+            raise customException(e, sys) from e
+
+    """
+    combining the above two methods to initiate data ingestion process
+    """
+
+    def initiate_data_ingestion(self):
+        try:
+            dataframe = self.export_collection_as_dataframe()
+            dataframe = self.export_data_to_feature_store(dataframe)
+            self.split_data_as_train_test(dataframe)
+            dataframe_ingestion_artifact = DataIngestionArtifact(
+                train_file_path=self.data_ingestion_config.training_file_path,
+                test_file_path=self.data_ingestion_config.testing_file_path,
+            )
+            return dataframe_ingestion_artifact
+        except Exception as e:
+            raise customException(e, sys) from e
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -1,31 +1,44 @@
 from datetime import datetime
 import os
-import os
+
 from networksecurity.constant import training_pipeline
 
 
 class TrainingPipelineConfig:
-    def __init__(self, timestamp=datetime.now()):
-        self.piepline_name = "networkSecurity"
-        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+    def __init__(self, timestamp: datetime = None):
+        if timestamp is None:
+            timestamp = datetime.now()
+        self.pipeline_name = "networkSecurity"
+        self.timestamp: str = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.artifact_name = training_pipeline.ARTIFACT_DIR
         self.artifact_dir = os.path.join(self.artifact_name, self.timestamp)
-        self.timestamp: str = timestamp
 
 
 class DataIngestionConfig:
-    def __init__(self, TrainingPipelineConfig: TrainingPipelineConfig):
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        # constants from training_pipeline
         self.database_name = training_pipeline.DATA_INGESTION_DATABASE_NAME
         self.collection_name = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.train_test_split_ratio = (
             training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         )
+
+        # build directories under the run-specific artifact dir
         self.data_ingestion_dir = os.path.join(
-            training_pipeline.ARTIFACT_DIR, training_pipeline.DATA_INGESTION_DIR_NAME
+            training_pipeline_config.artifact_dir,
+            training_pipeline.DATA_INGESTION_DIR_NAME,
         )
         self.feature_store_dir = os.path.join(
             self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR
         )
         self.ingested_dir = os.path.join(
             self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR
+        )
+
+        # file paths
+        self.training_file_path = os.path.join(
+            self.ingested_dir, training_pipeline.TRAIN_FILE_NAME
+        )
+        self.testing_file_path = os.path.join(
+            self.ingested_dir, training_pipeline.TEST_FILE_NAME
         )
