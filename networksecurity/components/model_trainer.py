d18b6caab9eed32a6ef1e1d24b77ac6cde394ed2
Ayham <ayhampt011@gmail.com>
2026-09-21 11:41:48 +0530

    Added model pusher implementation

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -136,6 +136,7 @@ class ModelTrainer:
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=Network_model,
             )
+            save_object("final_model/model.pkl", best_model)
 
             # Model traininer artifact
             model_trainer_artifact = ModelTrainerArtifact(
